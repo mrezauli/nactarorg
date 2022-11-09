f4e16e1e1a28792e55a75d4269e0ffb4349db318
Rezaul Islam <rezaul.cse.mbstu@gmail.com>
2022-11-09 17:23:34 +0600

    updated to orion

---
+++ app/Http/Controllers/Api/LaboratoriesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Laboratory;
+use Orion\Http\Controllers\Controller;
+use Orion\Concerns\DisableAuthorization;
+
+class LaboratoriesController extends Controller
+{
+    use DisableAuthorization;
+    /**
+     * Fully-qualified model class name
+     */
+    protected $model = Laboratory::class; // or "App\Models\Laboratory"
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use Orion\Facades\Orion;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LaboratoriesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['as' => 'api.'], function() {
+    Orion::resource('laboratories', LaboratoriesController::class);
 });
